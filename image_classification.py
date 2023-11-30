1306117394d4baee1ff3699afe2e0289d3a65662
tokarevsas31 <90484184+tokarevsas31@users.noreply.github.com>
2023-11-30 18:07:33 +0500

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -39,7 +39,7 @@ def print_predictions(preds):
 model = load_model()
 
 
-st.title('URFU SF классификации изображений в облаке Streamlit')
+st.title('классификации изображений в облаке Streamlit')
 img = load_image()
 result = st.button('Распознать изображение')
 if result:
